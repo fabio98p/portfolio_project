a4f6b38844771acc460cc8f4945930482fd8cc58
fabio98p <parisifabio98@gmail.com>
2022-09-24 01:26:21 +0200

    improved the style of project component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-    <div id="app">
-      <Nav />
-      <div class="container">
-        <router-view />
-      </div>
-      <Footer />
-    </div>
-  </template>
-  <script>
-  import Nav from "@/components/Nav"
-import Footer from "./components/Footer.vue";
-  export default {
-    components: { Nav, Footer }
-  }
-  </script>
-  <style>
-  body {
-    margin: 0;
-  }
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100vh;
-  }
-  #app .container {
-    padding: 1rem;
-  }
-  </style>
+	<div id="app">
+		<Nav />
+		<div class="container">
+			<router-view />
+		</div>
+		<Footer />
+	</div>
+</template>
+<script>
+import Nav from '@/components/Nav'
+import Footer from '@/components/Footer.vue'
+export default {
+	components: { Nav, Footer }
+}
+</script>
+<style>
+@import './master.scss';
+body {
+	margin: 0;
+}
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100vh;
+}
+#app .container {
+}
+</style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,50 +1,104 @@
 <template>
 	<section id="project">
-		<h1>{{ $t(`projects.${this.$route.params.id}.name`) }}</h1>
-        <div v-for=" tag in $tm(`projects.${this.$route.params.id}.tags`)">
-            {{tag.source}}
-        </div>
-		<h1>{{ $t(`projects.${this.$route.params.id}.title`) }}</h1>
-		<h1>{{ $t(`projects.${this.$route.params.id}.description`) }}</h1>
-		<h1>{{ $t(`projects.${this.$route.params.id}.technicaldescription`) }}</h1>
-		<a :href="$t(`projects.${this.$route.params.id}.githublink`)">
-			<font-awesome-icon icon="fa-brands fa-github" />
-		</a>
-        <button @click="test">log</button>
-        <YoutubePlayer />
-        <Carousel :datas="$tm(`projects.${this.$route.params.id}.images`)" />
-
+		<div>
+			<header>
+				<h1>{{ $t(`projects.${this.$route.params.id}.title`) }}</h1>
+				<div class="tags">
+					<div class="tag" :class="tag.source" v-for="tag in $tm(`projects.${this.$route.params.id}.tags`)">
+						{{ tag.source }}
+					</div>
+				</div>
+			</header>
+			<body>
+				<div class="subtitle">
+					<h2>{{ $t(`projects.${this.$route.params.id}.subtitle`) }}</h2>
+					<button @click="ChangeDescription">{{description}}</button>
+				</div>
+				<p v-if="description == 'description'">{{ $t(`projects.${this.$route.params.id}.description`) }}</p>
+				<p v-else>{{ $t(`projects.${this.$route.params.id}.technicaldescription`) }}</p>
+				<a :href="$t(`projects.${this.$route.params.id}.githublink`)">
+					<font-awesome-icon icon="fa-brands fa-github" />
+				</a>
+				<button @click="test">log</button>
+				<YoutubePlayer />
+				<Carousel :datas="$tm(`projects.${this.$route.params.id}.images`)" />
+			</body>
+		</div>
 	</section>
 </template>
 <script>
-import YoutubePlayer from "../components/YoutubePlayer.vue";
-import Carousel from "@/components/Carousel.vue";
+import YoutubePlayer from '../components/YoutubePlayer.vue'
+import Carousel from '@/components/Carousel.vue'
 export default {
 	name: 'project',
 	components: { YoutubePlayer, Carousel },
 	data() {
 		return {
-            images: ["ciccio", "pino", "filberto"],
-            imagesaaaa: this.$tm(`projects.${this.$route.params.id}.tags`),
-			componentKey: 0
+			images: ['ciccio', 'pino', 'filberto'],
+			imagesaaaa: this.$tm(`projects.${this.$route.params.id}.tags`),
+			description: 'technicaldescription'
 		}
 	},
 	methods: {
 		forceRerender() {
-            stopVideo()
-            console.log(this.componentKey);
+			stopVideo()
+			console.log(this.componentKey)
 			this.componentKey += 1
 		},
-        test(){
-            console.log(this.imagesaaaa);
-        }
+		test() {
+			console.log(this.imagesaaaa)
+		},
+		ChangeDescription() {
+			if (this.description == 'technicaldescription') {
+				this.description = 'description'
+			} else {
+				this.description = 'technicaldescription'
+			}
+		}
 	}
 }
 </script>
 <style scoped lang="scss">
+@import '../master.scss';
 #project {
-	h1 {
-		color: cadetblue;
+	width: 70%;
+	margin: 0 auto;
+	background-color: lightblue;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	header {
+		h1 {
+			font-size: 60px;
+			margin-bottom: 10px;
+			color: $black;
+		}
+		.tags {
+			display: flex;
+			flex-wrap: wrap;
+			.tag {
+				width: 60px;
+				height: 30px;
+				border-radius: 15px;
+				margin-right: 10px;
+
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+		}
+	}
+	body {
+		.subtitle {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		p {
+			font-size: 22px;
+		}
 	}
 }
 </style>
